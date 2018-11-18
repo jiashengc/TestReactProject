5739f10955ba6e78f26cc8f7f0ca50127ddf223c
Jia Sheng Chong <jiashengc@outlook.com>
2018-11-18 18:50:30 +1000

    Done tutorial till https://facebook.github.io/react-native/docs/network

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Alert, StyleSheet,  Text, TextInput, Button, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+class NavigationItem extends Component {
   render() {
+
+    return (
+      <Text>{this.props.name}</Text>
+    )
+  }
+}
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messageTyped: '',
+    };
+  }
+
+  render() {
+    let pic = {
+      uri: 'https://i.imgur.com/dWxjl35.jpg'
+    };
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
+        <Text>{this.state.messageTyped}</Text>
+
+        <View style={styles.navigationMenu}>
+          <NavigationItem name='Jennie'></NavigationItem>
+          <NavigationItem name='Jisoo'></NavigationItem>
+          <NavigationItem name='Lisa'></NavigationItem>
+          <NavigationItem name='Rose'></NavigationItem>
+        </View>
+
+        
+        <Image source={pic} style={{width: 200, height: 200}} />
+        
+        <TextInput 
+          style={{height: 40, backgroundColor: 'white'}}
+          placeholder="Type here to send a message"
+          onChangeText={(text) => this.setState({messageTyped: text})}>
+        </TextInput>
+
+        <Button
+          onPress={() => {
+            Alert.alert('You tapped a button!');
+          }}
+          title="Press ME"
+        ></Button>
+
       </View>
     );
   }
@@ -14,8 +58,19 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'powderblue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  navigationMenu: {
+    flexDirection: 'row',
+    color: 'gray',
+    justifyContent: 'space-between',
+    alignItems: 'stretch'
+  },
+  member: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 30,
   },
 });
